d808f9ed99f09d4e274c9ea4e42dedb868150921
yusufkrta25 <kotlinyusuf25@gmail.com>
2025-02-04 04:34:04 +0300

    Simple Calculator

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText number1, number2;
+    TextView result;
+    int sayi1 , sayi2, sonuc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,62 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        number1 = findViewById(R.id.number1View);
+        number2 = findViewById(R.id.number2View);
+        result = findViewById(R.id.resultView);
+
+    }
+
+    public void toplama(View view){
+        if (number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            result.setText("Enter Number");
+        }
+        else{
+            sayi1 = Integer.parseInt(number1.getText().toString());
+            sayi2 = Integer.parseInt(number2.getText().toString());
+            sonuc = sayi1 + sayi2;
+            result.setText(Integer.toString(sonuc));
+        }
+    }
+
+    public void cıkarma(View view){
+        if (number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            result.setText("Enter Number");
+        }
+        else{
+            sayi1 = Integer.parseInt(number1.getText().toString());
+            sayi2 = Integer.parseInt(number2.getText().toString());
+            sonuc = sayi1-sayi2;
+            result.setText(Integer.toString(sonuc));
+        }
+    }
+    public void bolme(View view){
+        if (number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            result.setText("Enter Number");
+        }
+        else{
+            sayi1 = Integer.parseInt(number1.getText().toString());
+            sayi2 = Integer.parseInt(number2.getText().toString());
+            if (sayi2 == 0){
+                result.setText("zero entering");
+            }
+            else{
+                sonuc = sayi1/sayi2;
+                result.setText(Integer.toString(sonuc));
+            }
+        }
+
+    }
+    public void carpma(View view ){
+        if (number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            result.setText("Enter Number");
+        }
+        else{
+            sayi1 = Integer.parseInt(number1.getText().toString());
+            sayi2 = Integer.parseInt(number2.getText().toString());
+            sonuc = sayi1*sayi2;
+            result.setText(Integer.toString(sonuc));
+        }
     }
 }
